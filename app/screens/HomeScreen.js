b57935b22207ac51ef45926bc425d87ab448034b
Jason <jason_9kh@yahoo.com>
2020-07-21 00:32:11 +0800

    added home screen body

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -3,20 +3,37 @@ import React from 'react';
 import { StyleSheet, View, Text, TextInput } from 'react-native';
 
 import commonStyles from '../styles/commonStyles';
-import Greeting from '../shared/Greeting'; 
+import Greeting from '../shared/Greeting';
 
 const HomeScreen = () => {
 
-  const[value, onChangeText] = React.useState("Enter Text"); 
-  console.log(value)
+  const [originValue, originOnChangeText] = React.useState("Enter Origin");
+  const [destValue, destOnChangeText] = React.useState("Enter Destination");
   return (
     <View style={styles.bodyContainer}>
       <Greeting />
       <Text style={styles.bodyTitle}>Where would you like to go? </Text>
-      <TextInput 
-      style={styles.textBox}
-      onChangeText={text => onChangeText(text)} 
-      value={value}/>
+      <View style={styles.inputSection}>
+        <View style={styles.textBoxInput}>
+          <View style={styles.textInputBorder}>
+            <TextInput
+              style={styles.inputField}
+              onChangeText={text => originOnChangeText(text)}
+              value={originValue} />
+          </View>
+        </View>
+        <View style={styles.textBoxInput}>
+          <View style={styles.textInputBorder}>
+            <TextInput
+              style={styles.inputField}
+              onChangeText={text => destOnChangeText(text)}
+              value={destValue} />
+          </View>
+        </View>
+      </View>
+      <View style={{flex: 1}}>
+        <Text>Another section</Text>
+      </View>
     </View>
   )
 }
@@ -34,8 +51,26 @@ const styles = StyleSheet.create({
     paddingTop: '4%',
     paddingLeft: '5%'
   },
-  textBox: {
-
+  inputSection: {
+    flex: 1,
+    height: '20%'
+  },
+  textBoxInput: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: '3%'
+  },
+  textInputBorder: {
+    borderBottomColor: commonStyles.themeOrange,
+    borderBottomWidth: 1,
+    width: '70%',
+  },
+  inputField: {
+    fontSize: 18,
+    alignSelf: 'auto',
+    color: commonStyles.textBrown,
+    fontFamily: 'SourceSansPro-Regular',
+    marginLeft: 10
   }
 })
 
--- app/styles/commonStyles.js
+++ app/styles/commonStyles.js
@@ -1,5 +1,6 @@
 export default{
     backgroundOrange: '#FFA640',
+    themeOrange: '#E98918',
     textBrown: '#462716',
     white: '#FFFFFF'
 }
